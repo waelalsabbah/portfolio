9d1c65729662f5d596cc0edb5e743fe7c539b544
waelalsabbah <alsabbahwael@gmail.com>
2023-09-04 17:54:59 +0200

    change i# Your branch is up to date with 'origin/main'.

--- pages/index.jsx
+++ pages/index.jsx
@@ -15,7 +15,7 @@ function Home() {
         <div className="container">
           <div className="text-wrapper">
             <h1 data-test-id="pageTitle" className="title">
-              Halo, saya Lutfhi Ramadhan
+              Hallo, Wael
             </h1>
             <p className="description">
               Saya seorang pekerja lepas pengembang web, fotograper, desainer
